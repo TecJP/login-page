91cfd1290d3c1ab767c5f38c357542159aef38bb
TecJP <tec.joao.freitas@gmail.com>
2022-05-23 18:44:42 -0300

    Fixing home style

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { Heading, Stack, Button, Text } from "@chakra-ui/react"
+import { Heading, Stack, Button, Text, Container } from "@chakra-ui/react"
 import { useNavigate } from "react-router";
 import { useAuth } from "../../hooks/useAuth";
 
@@ -12,19 +12,21 @@ export function Dashboard() {
   }
 
   return (
-    <Stack>
-      <Heading mb="6">
-        Home
-      </Heading>
-      <Text colorScheme="teal">
-        {user?.name}
-      </Text>
-      <Text colorScheme="teal">
-        {user?.email}
-      </Text>
-      <Button mt="6" colorScheme="teal" onClick={handleSignOut}>
-        Sair
-      </Button>
+    <Stack direction='column'>
+      <Container maxW='xl'>
+        <Heading maxW='xl' mb="6">
+          Home
+        </Heading>
+        <Text colorScheme="teal">
+          {user?.name}
+        </Text>
+        <Text colorScheme="teal">
+          {user?.email}
+        </Text>
+        <Button width='100%' mt="6" colorScheme="teal" onClick={handleSignOut}>
+          Sair
+        </Button>
+      </Container>
     </Stack>
   );
 }
